8424371d39b2796ebdacde1909ee805d6e1e34da
Greg Bacus <gbacus28@gmail.com>
2017-06-08 12:50:34 -0500

    added letter component

--- app/components/AttorneyProfile.jsx
+++ app/components/AttorneyProfile.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import * as Redux from 'react-redux';
 import Navigation from 'Navigation';
 import Footer from 'Footer';
+import Letter from 'Letter';
 
 
 //import * as actions from 'actions';
@@ -11,38 +12,70 @@ export var AttorneyProfile = React.createClass({
   render() {
 
     return(
+
+
       <div className="sections">
         <section id="profile" data-magellan-target="profile">
           <div className="callout small main">
-            <div className="row">
+            <div className="row callout success">
               <div className="small-12 medium-12 large-12 columns textCenter">
                 <h1>Welcome to Bacus Law</h1>
                 <h4>Eric Bacus, Immigration Attorney</h4>
-                <h6 className="ital">Se Habla Español</h6>
               </div>
             </div>
+            <br></br>
             <div className="row">
-              <div className="small-12 medium-12 large-12 columns">
+              <div className="small-6 medium-6 large-6 columns">
                 <img className="ericResize" src="/assets/eric.jpg" alt="Bacus Law Logo"/>
+                <div className="callout primary callToday">
+                  <h5>Call today for a free consultation.</h5>
+                  <h6 className="ital">Se Habla Español</h6>
+                  <h5><a className="boldNumber" href="tel:+1-832-910-7923">(832) 910-7923</a></h5>
+                </div>
               </div>
-            </div>
-            <div className="row">
-              <div className="small-12 medium-12 large-12 columns textCenter">
-                <h5>Call today for a free consultation.</h5>
-                <h5><a className="boldNumber" href="tel:+1-832-910-7923">(832) 910-7923</a></h5>
-                <a
-                  href="https://www.linkedin.com/in/eric-bacus-8b08224/"
-                  target="_blank"
-                  className="linkedInResize">
-                  <img src='/assets/Logo-2C-128px-R.png' alt="linkedIn logo"/>
-                </a>
+              <div className="small-6 medium-6 large-6 columns">
+                <Letter />
               </div>
             </div>
           </div>
         </section>
       </div>
+
     )
   }
 })
 
 export default Redux.connect()(AttorneyProfile);
+
+
+     // {<div className="sections">
+     //    <section id="profile" data-magellan-target="profile">
+     //      <div className="callout small main">
+     //        <div className="row">
+     //          <div className="small-12 medium-12 large-12 columns textCenter">
+     //            <h1>Welcome to Bacus Law</h1>
+     //            <h4>Eric Bacus, Immigration Attorney</h4>
+     //            <h6 className="ital">Se Habla Español</h6>
+     //          </div>
+     //        </div>
+     //        <div className="row">
+     //          <div className="small-12 medium-12 large-12 columns">
+     //            <img className="ericResize" src="/assets/eric.jpg" alt="Bacus Law Logo"/>
+     //          </div>
+     //        </div>
+     //        <div className="row">
+     //          <div className="small-12 medium-12 large-12 columns textCenter">
+     //            <h5>Call today for a free consultation.</h5>
+     //            <h5><a className="boldNumber" href="tel:+1-832-910-7923">(832) 910-7923</a></h5>
+     //            <a
+     //              href="https://www.linkedin.com/in/eric-bacus-8b08224/"
+     //              target="_blank"
+     //              className="linkedInResize">
+     //              <img src='/assets/Logo-2C-128px-R.png' alt="linkedIn logo"/>
+     //            </a>
+     //          </div>
+     //        </div>
+     //      </div>
+     //      <Letter />
+     //    </section>
+     //  </div>}
---
+++ app/components/Letter.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import * as Redux from 'react-redux';
+
+
+export var Letter = React.createClass({
+
+
+
+  render() {
+
+    return(
+      <div className="callout secondary letter">
+        <p>Thank you for your interest in Bacus Law.  I am the owner and sole attorney.  I am passionate about helping people who need to interact with the US immigration system by providing legal advice and representation with excellence and compassion.</p>
+
+        <p>If you choose to work with me, I will do my best to ensure that you, the client, experience the following:</p>
+
+        <ol>
+          <li>You understand your situation and your options, based on timely and accurate legal advice.</li>
+          <li>You set the goals of the legal representation, and I carefully execute the best strategy for meeting those goals.</li>
+          <li>In the end, you would unreservedly recommend Bacus Law to your friends, family and loved ones.</li>
+        </ol>
+
+        <p>As with any attorney, I cannot guarantee you a particular result.  But I can guarantee you my zealous dedication to your case.  I look forward to working with you, and please do get in touch if I can answer any questions.</p>
+
+        <p>
+          Sincerely, <br />
+          -signature- <br />
+          Eric Bacus <br />
+        </p>
+      </div>
+    )
+
+  }
+})
+
+export default Redux.connect()(Letter);
